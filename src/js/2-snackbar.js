add0ba8f10f989c0aa18bd8f23e2ae6d9e385b3c
Oleh Zakharchenko <164777305+Oleh-Zakharchenko@users.noreply.github.com>
2024-08-13 16:13:50 +0200

    done-hw-10

---
+++ src/js/2-snackbar.js
@@ -0,0 +1,40 @@
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+
+iziToast.settings({
+  position: "topRight",
+  layout: 1,
+});
+
+const form = document.querySelector(".form");
+
+form.addEventListener("submit", function(event) {
+  event.preventDefault(); 
+
+  const formData = new FormData(this);
+  const delay = formData.get("delay");
+  const state = formData.get("state");
+
+  const promise = new Promise((resolve, reject) => {
+    if (state === "fulfilled") {
+      setTimeout(() => resolve(delay), delay);
+    } else if (state === "rejected") {
+      setTimeout(() => reject(delay), delay);
+    }
+  });
+
+  promise.then(
+   
+    (delay) => {
+      iziToast.success({
+        message: `✅ Fulfilled promise in ${delay}ms`,
+      });
+    },
+  
+    (delay) => {
+      iziToast.error({
+        message: `❌ Rejected promise in ${delay}ms`,
+      });
+    }
+  );
+});
